34f8bcf6af3a1e8df21bb87cdb0faadb42bb8bea
Adam Reed <kusa.xisa@gmail.com>
2017-01-24 00:08:59 -0500

    Added emails method

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -1,5 +1,13 @@
+# Pulls data from a selection of text
 class Extractor
   def phone_numbers(text)
+    # Looks for a phone number in the format of (123) 456-7890
     text.scan(/\(\d{3}\) \d{3}-\d{4}/)
+  end
+
+  def emails(text)
+    # Looks for any chars that might or might not have period separators,
+    # followed by @, then more letters, a period, and at least more two letters
+    text.scan(/(?:\w+\.)*\w+@\w+\.\w{2,}/)
   end
 end
